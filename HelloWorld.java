6598359276db1c22ec4007e0dbfcd9889aa45dbe
RomKanKan <109809205+RomKanKan@users.noreply.github.com>
2022-09-15 17:09:31 +0300

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,8 +7,11 @@ public class HelloWorld {
 		String message = "Hello World!";
 		String words = "Java forever";
 		String space = " ";
+		int i = 42;
+		int j = 3;
 				
 		System.out.println(message + space + words);
+		System.out.println(i + j);
 
 	}
 
