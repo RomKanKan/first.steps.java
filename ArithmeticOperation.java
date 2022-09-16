361b5e1d6617850ab4fce7b620cfdf6d454d4a0a
RomKanKan <109809205+RomKanKan@users.noreply.github.com>
2022-09-16 17:12:39 +0300

    Add files via upload

---
+++ ArithmeticOperation.java
@@ -0,0 +1,15 @@
+package start.java.operation.arithmetic;
+
+public class ArithmeticOperation {
+
+	public static void main(String[] args) {
+		
+		float i = 6;
+		float j = 7;
+		float answer = i / j;
+		
+		System.out.println(answer);
+
+	}
+
+}
